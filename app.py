3e44bb3ea96959543a0c282fdc76648ba4a87018
GabrielModesto <gabrieliba@gmail.com>
2022-10-14 21:44:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,25 +24,39 @@ with col1:st.write(' ')
 with col2:st.image("htp.png") 
 with col3:st.write(' ')
 
-st.warning('Valor pronóstico de la relación ecocardiográfica de excursión sistólica del plano anular tricuspídeo/presión sistólica de la arteria pulmonar (TAPSE/sPAP) para la evaluación del riesgo de HAP no invasiva.')
+def main():
+
+ st.warning('Valor pronóstico de la relación ecocardiográfica de excursión sistólica del plano anular tricuspídeo/presión sistólica de la arteria pulmonar (TAPSE/sPAP) para la evaluación del riesgo de HAP no invasiva.')
 
 
-tapse= st.selectbox('TAPSE/sPAP >0.33 mm/mmHg',("Escoja valor", "Si", "No"), key = "1")
+ with st.form(key='myform',clear_on_submit=True):
+
+          
+    tapse= st.selectbox('TAPSE/sPAP >0.33 mm/mmHg',("Escoja valor", "Si", "No"), key = "1")
     
-nyha = st.selectbox("NYHA I-II", ("Escoja valor","Si", "No"), key = "2")
+    nyha = st.selectbox("NYHA I-II", ("Escoja valor","Si", "No"), key = "2")
     
-bnp = st.selectbox("NTproBNP <300 ng/L o BNP <50 ng/L", ("Escoja valor","Si", "No"), key = "3")
+    bnp = st.selectbox("NTproBNP <300 ng/L o BNP <50 ng/L", ("Escoja valor","Si", "No"), key = "3")
 
-
-if tapse=="Escoja valor" and nyha=="Escoja valor" and bnp=="Escoja valor":
-    pass
-elif tapse=="Si" and nyha=="Si" and bnp=="Si":
-    st.info('TAPSE/sPAP >0,33 mm/mmHg, NYHA I-II y NTproBNP <300 ng/L o BNP <50 ng/L permiten identificar pacientes con HAP de bajo riesgo en el seguimiento')
-else:
-    st.info('Evalue variables cínicas, de laboratorio y ecocardiografícas para determinar si su paciente está en riesgo intermedio o alto')
+    submit_button =st.form_submit_button("Evaluar")
     
-primary_clr = st.get_option("theme.primaryColor")
-txt_clr = st.get_option("theme.textColor")
+    st.write("[Ampliar información](https://www.sciencedirect.com/science/article/abs/pii/S1053249822021143)")
+
+    if submit_button:
+   
+      if tapse=="Escoja valor" and nyha=="Escoja valor" and bnp=="Escoja valor":
+         pass
+     
+      elif tapse=="Si" and nyha=="Si" and bnp=="Si":
+          st.write('TAPSE/sPAP >0,33 mm/mmHg, NYHA I-II y NTproBNP <300 ng/L o BNP <50 ng/L permiten identificar pacientes con HAP de bajo riesgo en el seguimiento')
+    
+      elif  tapse=="No" or nyha=="No" or bnp=="No":
+          st.write('Evalue variables cínicas, de laboratorio y ecocardiografícas para determinar si su paciente está en riesgo intermedio o alto')
+    
+if __name__ == '__main__':
+    # This code won't run if this file is imported.
+    main()
+
 
 components.html(
     """
@@ -58,12 +72,16 @@ elements[0].style.backgroundColor = 'lightblue'
       
 col1, col2 = st.columns(2) 
 with col1:st.write(' ') 
-if st.button('Reiniciar'):
 
-  with col2:st.write(' ')
+# if st.button("Reset"):
   
   
-st.write("[Ampliar información](https://www.sciencedirect.com/science/article/abs/pii/S1053249822021143)")
+
+    
+    
+    
+
+
     
     
     
